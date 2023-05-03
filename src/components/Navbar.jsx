5d8adb22537b79f4b8440a347b505614c14f2572
Nyakamba <omwegaenock@gmail.com>
2023-05-03 13:36:23 +0300

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useState } from "react";
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../constants";
+
+function Navbar() {
+  return;
+  <nav className="w-full flex py-6 justify-between items-center navbar"></nav>;
+}
+
+export default Navbar;
