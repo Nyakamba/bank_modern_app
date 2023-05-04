e2fcec26a6e6d1d10a0d9405d0dca252b1406d80
Nyakamba <omwegaenock@gmail.com>
2023-05-04 12:22:21 +0300

    Business.jsx

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -9,15 +9,25 @@ const FeatureCard = ({ icon, title, content, index }) => (
       index !== features.length - 1 ? "mb-6" : "mb-0"
     } feature-card`}
   >
-    <div>
-      <img src={icon} alt="icon" />
+    <div
+      className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}
+    >
+      <img src={icon} alt="star" className="w-[50%] h-[50%] object-contain" />
+    </div>
+    <div className="flex-1 flex flex-col ml-3">
+      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23.4px] mb-1">
+        {title}
+      </h4>
+      <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px]">
+        {content}
+      </p>
     </div>
   </div>
 );
 
 function Business() {
   return (
-    <section id="features">
+    <section id="features" className={layout.section}>
       {" "}
       <div className={layout.sectionInfo}>
         <h2 className={styles.heading2}>
@@ -33,7 +43,7 @@ function Business() {
       </div>
       <div className={`${layout.sectionImg} flex-col `}>
         {features.map((feature, index) => (
-          <FeatureCard key={feature.id} {...features} index={index} />
+          <FeatureCard key={feature.id} {...feature} index={index} />
         ))}
       </div>
     </section>
