441d34d2eda24116798b52e52fa9f3e6b03e163e
Nyakamba <omwegaenock@gmail.com>
2023-05-04 15:20:44 +0300

    bill.jsx

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { apple, bill, google } from "../assets";
+import styles, { layout } from "../styles";
 
 export default function Billing() {
-  return <div>Billing</div>;
+  return (
+    <section id="product" className={layout.sectionReverse}>
+      <div className={layout.sectionReverse}>
+        <img
+          src={bill}
+          alt="bill"
+          className="w-[100%] h-[100%] relative z-[5]"
+        />
+      </div>
+    </section>
+  );
 }
