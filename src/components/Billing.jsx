9c860b7596a7172c21d43c1cc378c5d881be31f8
Nyakamba <omwegaenock@gmail.com>
2023-05-04 15:56:40 +0300

    Billing.jsx

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -15,6 +15,15 @@ export default function Billing() {
         <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient " />
         <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient " />
       </div>
+
+      <div>
+        <h2></h2>
+        <p></p>
+        <div>
+          <img />
+          <img />
+        </div>
+      </div>
     </section>
   );
 }
