8de35856097aa707bf75221984ec81bcb446772a
Nyakamba <omwegaenock@gmail.com>
2023-05-04 11:12:08 +0300

    button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-function Button() {
-  return <div>Button</div>;
+function Button({ styles }) {
+  return (
+    <button
+      type="button"
+      className={`py-4 px-6 bg-blue-gradient text-[18px] txt-primary outline-none ${styles}`}
+    >
+      Get Started
+    </button>
+  );
 }
 
 export default Button;
