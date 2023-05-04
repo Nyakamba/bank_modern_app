fc2d1fa9147c74f89b89274a7d31b3f52b0a44e5
Nyakamba <omwegaenock@gmail.com>
2023-05-04 12:03:00 +0300

    button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ function Button({ styles }) {
   return (
     <button
       type="button"
-      className={`py-4 px-6 bg-blue-gradient text-[18px] txt-primary outline-none ${styles}`}
+      className={`py-4 px-6 bg-blue-gradient text-[18px] text-primary outline-none ${styles}`}
     >
       Get Started
     </button>
